581531a52b24e6062ed46f9b06da703cd42f9113
Adrian Guerrero amelin <aguerrer@c3r10s5.42madrid.com>
2021-07-21 17:57:59 +0200

    mie-2021/07/2021

--- src/checker/includes/checker.h
+++ src/checker/includes/checker.h
@@ -6,7 +6,7 @@
 /*   By: aguerrer </var/mail/aguerrer>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/19 22:08:24 by aguerrer          #+#    #+#             */
-/*   Updated: 2021/07/21 15:10:36 by aguerrer         ###   ########.fr       */
+/*   Updated: 2021/07/21 17:26:15 by aguerrer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,35 +41,22 @@ typedef struct s_program
 }					t_program;
 
 int					stack_contains(t_stack *stack, int num);
-
 int					init_stacks(int argc, char *argv[], \
 									t_stack *stack_a, t_stack *stack_b);
-
 void				reverse_rotate_stack(t_stack *stack);
 void				rotate_stack(t_stack *stack);
 void				push_stack(t_stack *stack1, t_stack *stack2);
 void				swap_stack(t_stack *stack);
-
 void				free_instructions(t_instruction *instructions);
 void				execute_instructions(t_instruction *instr, \
 	   					t_stack *stack_a, t_stack *stack_b);
 int					add_instruction(t_instruction **intructions, char *line);
 int					get_instruction(t_instruction **instructions);
-
 int					ft_strcmp(const char *s1, const char *s2);
 char				*ft_strdup(const char *str);
 size_t				ft_strlen(const char *s);
 void				ft_putnbr(int n);
 int					ft_atoi(const char *str, int *num);
-
-void				print_stacks(t_stack *stack_a, t_stack *stack_b);
-void				print_instructions(t_instruction *instructions);
-void				print_debug_instruction(t_instruction *instr, \
-								t_stack *stack_a, t_stack *stack_b);
-
-void				write_n_char(int n, char c);
-int					size_nbr(int n);
-int					get_max_size_nbr_stack(t_stack *stack);
 int					free_prg(t_program *prg);
 
 #endif
